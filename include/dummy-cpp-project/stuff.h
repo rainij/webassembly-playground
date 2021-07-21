2f6e51d1dcf113ad544899c81100721e705d5de1
Reinhard Stahn <reinhardstahn@t-online.de>
2021-07-21 17:43:54 +0200

    Initial commit

---
+++ include/dummy-cpp-project/stuff.h
@@ -0,0 +1,8 @@
+#ifndef STUFF_H
+#define STUFF_H
+
+namespace dummy_cpp_project {
+    void say_hello();
+}
+
+#endif // STUFF_H
---
+++ src/stuff.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "dummy-cpp-project/stuff.h"
+
+using namespace dummy_cpp_project;
+
+void say_hello() {
+    printf("Hello Sir!");
+}
